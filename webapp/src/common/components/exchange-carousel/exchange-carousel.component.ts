824d4757cc68509145bc92a82ea346a8e0aed3d9
jpvanderlei <joaop.vanderlei@gmail.com>
2024-11-28 15:51:57 -0300

    last changes

---
+++ webapp/src/common/components/exchange-carousel/exchange-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-exchange-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './exchange-carousel.component.html',
+  styleUrl: './exchange-carousel.component.scss'
+})
+export class ExchangeCarouselComponent {
+
+}
--- webapp/src/common/components/home/home.component.ts
+++ webapp/src/common/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { BannerComponent } from '../banner/banner.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, BannerComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
